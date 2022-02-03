c64cec78571f0ab5be03cd143c6a1a73cf3b4cf7
supernlogn <ath.ioannis94@gmail.com>
2022-02-03 19:56:26 +0200

    remove getMeshNormals from tracker

--- panoptic_mapping/src/tracking/projective_id_tracker.cpp
+++ panoptic_mapping/src/tracking/projective_id_tracker.cpp
@@ -265,48 +265,6 @@ ProjectiveIDTracker::seperateBackgroundIds(const std::vector<int> input_ids) {
   return std::make_pair(foreground_ids, background_ids);
 }
 
-float getMeshNormals(Submap* submap) {
-  auto mesh_layer = submap->getMeshLayerPtr();
-  voxblox::BlockIndexList mesh_indices;
-  mesh_layer->getAllUpdatedMeshes(
-      &mesh_indices);  // TODO(supernlogn): See if this is not needed
-  LOG(INFO) << "mesh_indices.size() =" << mesh_indices.size();
-  voxblox::Pointcloud floor_vertices;
-  voxblox::Point up(0, 1.0, 0.0);
-  float up_threshold = 0.8;
-  // const int class_id = submap->getClassID();
-  const auto& classLayer = submap->getClassLayer();
-  for (const voxblox::BlockIndex& block_index : mesh_indices) {
-    voxblox::Mesh::Ptr mesh = mesh_layer->getMeshPtrByIndex(block_index);
-    const panoptic_mapping::ClassBlock& class_block =
-        classLayer.getBlockByIndex(block_index);
-    // TODO(supernlogn) clustering can be done with label also
-    for (size_t i = 0u; i < mesh->vertices.size(); ++i) {
-      const auto pn = mesh->normals[i];
-      const auto& class_voxel = class_block.getVoxelByLinearIndex(i);
-      std::cout << pn << ";";
-      float dot_prod = pn.dot(up);
-      if (dot_prod > up_threshold) {
-        floor_vertices.push_back(mesh->vertices[i]);
-      }
-    }
-    std::cout << '\n';
-    std::cout << "mesh->vertices.size() = " << mesh->vertices.size() << '\n';
-  }
-
-  double mean_vertice_y = 0.0;
-  for (const auto vert : floor_vertices) {
-    mean_vertice_y += vert.y();
-  }
-  if (floor_vertices.size() > 0) {
-    mean_vertice_y /= floor_vertices.size();
-  } else {
-    LOG(WARNING) << "No floor vertices pointing up";
-  }
-
-  return static_cast<float>(mean_vertice_y);
-}
-
 void ProjectiveIDTracker::processBackgroundInput(
     SubmapCollection* submaps, InputData* input,
     std::unordered_map<int, int>* input_to_output,
