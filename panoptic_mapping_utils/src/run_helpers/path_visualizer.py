3c2ee2f1ab5a94818207ca27f417661c102357dd
supernlogn <ath.ioannis94@gmail.com>
2022-02-03 20:44:44 +0200

    path visualizer without use_noise parameter

--- panoptic_mapping_utils/src/run_helpers/path_visualizer.py
+++ panoptic_mapping_utils/src/run_helpers/path_visualizer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import numpy as np
-
 import rospy
-from geometry_msgs.msg import PoseStamped, TransformStamped, Pose, Transform
+from geometry_msgs.msg import TransformStamped, Pose
 from visualization_msgs.msg import Marker
+
+import numpy as np
 
 
 class PathVisualizer(object):
@@ -18,74 +18,24 @@ class PathVisualizer(object):
         self.width = rospy.get_param('~width', 0.03)  # m
         self.distance = rospy.get_param('~distance', 0.05)  # m
         self.use_arrow = rospy.get_param('~use_arrow', True)  # m
-        self.use_noise = rospy.get_param('~use_noise', False)
         # ROS
-        if (self.use_noise):
-            self.sub = rospy.Subscriber("~pose_in", TransformStamped,
-                                        self.transform_cb)
-        else:
-            self.sub = rospy.Subscriber("~pose_in", PoseStamped, self.pose_cb)
-
-        if self.use_noise:
-            self.pub = rospy.Publisher("~path_noisy", Marker, queue_size=10)
-        else:
-            self.pub = rospy.Publisher("~path_gt", Marker, queue_size=10)
+        self.sub = rospy.Subscriber("~pose_in", TransformStamped,
+                                    self.transform_cb)
+        self.pub = rospy.Publisher("~marker_out", Marker, queue_size=10)
         # variables
         self.previous_pose = None
         self.counter = 0
 
-    def pose_cb(self, pose_stamped: PoseStamped):
+    def transform_cb(self, transform_stamped):
         # Init
         if self.previous_pose is None:
-            self.previous_pose = pose_stamped.pose
-            return
-
-        # Only plot every 'distance' meters
-        pose = pose_stamped.pose
-        dist = np.linalg.norm(
-            np.array([
-                self.previous_pose.position.x, self.previous_pose.position.y,
-                self.previous_pose.position.z
-            ]) - np.array([pose.position.x, pose.position.y, pose.position.z]))
-        if dist < self.distance:
-            return
-        self.previous_pose = pose_stamped.pose
-
-        # Plot
-        msg = Marker()
-        msg.header = pose_stamped.header
-        msg.pose = pose
-        msg.color.r = self.red
-        msg.color.g = self.green
-        msg.color.b = self.blue
-        msg.color.a = 1
-        msg.ns = "path"
-        if self.use_arrow:
-            msg.type = 0
-            msg.scale.x = self.length
-            msg.scale.y = self.width
-            msg.scale.z = self.width * 2
-        else:
-            # Sphere
-            msg.type = 2
-            msg.scale.x = self.width
-            msg.scale.y = self.width
-            msg.scale.z = self.width
-        msg.id = self.counter
-        self.counter = self.counter + 1
-        self.pub.publish(msg)
-
-    def transform_cb(self, transform_stamped: TransformStamped):
-        # Init
-        if self.previous_pose is None:
-            self.previous_pose = PathVisualizer.transformToPose(
+            self.previous_pose = self.transformToPose(
                 transform_stamped.transform)
             return
 
         # Only plot every 'distance' meters
 
-        pose: Pose = PathVisualizer.transformToPose(
-            transform_stamped.transform)
+        pose = self.transformToPose(transform_stamped.transform)
         dist = np.linalg.norm(
             np.array([
                 self.previous_pose.position.x, self.previous_pose.position.y,
@@ -93,8 +43,7 @@ class PathVisualizer(object):
             ]) - np.array([pose.position.x, pose.position.y, pose.position.z]))
         if dist < self.distance:
             return
-        self.previous_pose = PathVisualizer.transformToPose(
-            transform_stamped.transform)
+        self.previous_pose = pose
 
         # Plot
         msg = Marker()
@@ -121,7 +70,7 @@ class PathVisualizer(object):
         self.counter = self.counter + 1
         self.pub.publish(msg)
 
-    def transformToPose(transform: Transform) -> Pose:
+    def transformToPose(self, transform):
         p = Pose()
         p.position.x = transform.translation.x
         p.position.y = transform.translation.y
