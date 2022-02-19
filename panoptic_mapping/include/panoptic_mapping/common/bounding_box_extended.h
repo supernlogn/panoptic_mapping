1c1b61d90c4774c20ba9c10689cb3a66c7816f5d
supernlogn <ath.ioannis94@gmail.com>
2022-02-19 14:23:45 +0200

    bbox extended + plane type source files

--- panoptic_mapping/include/panoptic_mapping/common/bounding_box_extended.h
+++ panoptic_mapping/include/panoptic_mapping/common/bounding_box_extended.h
@@ -18,74 +18,20 @@ class BoundingBoxExtended : public voxgraph::BoundingBox {
  public:
   BoundingBoxExtended() = default;
   ~BoundingBoxExtended() = default;
-  void updateBoundingBoxLimits(const Point& p) {
-    min.x() = std::min(p.x(), min.x());
-    max.x() = std::max(p.x(), max.x());
-    min.y() = std::min(p.y(), min.y());
-    max.y() = std::max(p.y(), max.y());
-    min.z() = std::min(p.z(), min.z());
-    max.z() = std::max(p.z(), max.z());
-  }
+  void updateBoundingBoxLimits(const Point& p);
   /**
    * @brief Computes the intersection over Union between this box
    * and another one
    * @param other_bounding_box the other bounding box
    * @return float IoU metric
    */
-  float IoU(voxgraph::BoundingBox* other_bounding_box) const {
-    float union1 = computeVolume(max, min);
-    float union2 =
-        computeVolume(other_bounding_box->max, other_bounding_box->min);
-    float xA = std::max(min.x(), other_bounding_box->min.x());
-    float yA = std::max(min.y(), other_bounding_box->max.y());
-    float zA = std::max(min.z(), other_bounding_box->max.z());
-    float xB = std::min(max.x(), other_bounding_box->max.x());
-    float yB = std::min(max.y(), other_bounding_box->max.y());
-    float zB = std::min(max.z(), other_bounding_box->max.z());
-    float interVolume =
-        max(0, xB - xA + 1) * max(0, yB - yA + 1) * max(0, zB - zA + 1);
-    float total_union = union1 + union2;
-    assert(interVolume >= 0.0);
-    assert(total_union >= 0.0);
-    if (total_union == 0) {
-      return 0.0;
-    }
-    return interVolume / total_union;
-  }
+  float IoU(voxgraph::BoundingBox* other_bounding_box) const;
 
-  BoundingBoxMsg toBoundingBoxMsg() const {
-    BoundingBoxMsg ret;
-    tf::pointEigenToMsg(max.cast<double>(), ret.max);
-    tf::pointEigenToMsg(min.cast<double>(), ret.min);
-    return std::move(ret);
-  }
+  BoundingBoxMsg toBoundingBoxMsg() const;
 
-  static BoundingBoxExtended fromMsg(const BoundingBoxMsg& msg) {
-    BoundingBoxExtended be;
-    Eigen::Vector3d max_d, min_d;
-    tf::pointMsgToEigen(msg.max, max_d);
-    tf::pointMsgToEigen(msg.min, min_d);
-    be.max = max_d.cast<float>();
-    be.min = min_d.cast<float>();
-    return be;
-  }
+  static BoundingBoxExtended fromMsg(const BoundingBoxMsg& msg);
 
-  visualization_msgs::Marker getVisualizationMsg() {
-    visualization_msgs::Marker ret;
-    // visualize only 8 edges
-    for (int i = 0; i < 8; ++i) {
-      Point p_f;
-      p_f.x() = (!(i & 0b1)) ? min.x() : max.x();
-      p_f.y() = (!(i & 0b10)) ? min.y() : max.y();
-      p_f.z() = (!(i & 0b100)) ? min.z() : max.z();
-      geometry_msgs::Point p_m;
-      tf::pointEigenToMsg(p_f.cast<double>(), p_m);
-      ret.points.push_back(p_m);
-    }
-    ret.header.frame_id = "world";
-    ret.header.stamp = ros::Time::now();
-    return ret;
-  }
+  visualization_msgs::Marker getVisualizationMsg() const;
 
  protected:
   float computeVolume(const Point& max, const Point& min) const {
--- panoptic_mapping/include/panoptic_mapping/common/plane_type.h
+++ panoptic_mapping/include/panoptic_mapping/common/plane_type.h
@@ -18,50 +18,19 @@ class PlaneType {
  public:
   typedef size_t PlaneID;
   explicit PlaneType(const Eigen::Vector3f& normal, const Point& point,
-                     int class_id)
-      : plane_(normal, normal.dot(point)),
-        point_(point),
-        class_id_(class_id),
-        plane_id_(getNextPlaneID()) {
-    assert(normal.squaredNorm() - 1 < 1.000001 &&
-           1 - normal.squaredNorm() > 0.9999);
-    buildPlaneOrientation();
-    T_M_P_init_ = T_M_P_;
-    num_points_ = 0;
-  }
-  explicit PlaneType(const Eigen::Hyperplane<float, 3>& plane, int class_id)
-      : plane_(plane), class_id_(class_id), plane_id_(getNextPlaneID()) {
-    const auto& n = plane.normal();
-    point_ = plane.offset() * n;
-    buildPlaneOrientation();
-    T_M_P_init_ = T_M_P_;
-    num_points_ = 0;
-  }
+                     const int class_id);
+  explicit PlaneType(const Eigen::Hyperplane<float, 3>& plane, int class_id);
   explicit PlaneType(const Eigen::Hyperplane<float, 3>& plane,
-                     const Transformation& T_M_P, int class_id)
-      : plane_(plane), class_id_(class_id), plane_id_(getNextPlaneID()) {
-    const auto& n = plane.normal();
-    point_ = plane.offset() * n;
-    T_M_P_ = T_M_P;
-    T_M_P_init_ = T_M_P;
-    num_points_ = 0;
-  }
+                     const Transformation& T_M_P, int class_id);
   explicit PlaneType(const Eigen::Vector3f& normal, const Point& point,
-                     const Transformation& T_M_P, int class_id)
-      : PlaneType(Eigen::Hyperplane<float, 3>(normal, point), T_M_P, class_id) {
-    assert(normal.squaredNorm() - 1 < 1.000001 &&
-           1 - normal.squaredNorm() > 0.9999);
-  }
+                     const Transformation& T_M_P, int class_id);
   // getters
-  Eigen::Vector3f getPlaneNormal() const {
-    return plane_.normal().normalized();
-  }
-  Point getPointInit() const { return point_; }
-  Transformation getPlaneTransformation() const { return T_M_P_; }
-  PlaneID getPlaneID() const { return plane_id_; }
-  const voxgraph::BoundingBox* getBoundingBox() const {
-    return &planeSegmentAaBb_;
-  }
+  Eigen::Vector3f getPlaneNormal() const;
+  Point getPointInit() const;
+  Transformation getPlaneTransformation() const;
+  PlaneID getPlaneID() const;
+  const voxgraph::BoundingBox* getBoundingBox() const;
+  visualization_msgs::Marker getVisualizationMsg() const;
   // transformation accessors
   /**
    * @brief Plane orientation is based on the world frame so a transformation
@@ -70,100 +39,21 @@ class PlaneType {
    * @param Tnew_old
    */
   void transformPlane(const Transformation& Tnew_old,
-                      const Transformation& T_M_R) {
-    Transformation T_P_P;
-    transformPlane(T_P_P);
-  }
-  void transformPlane(const Transformation& Tnew_old) {
-    T_M_P_ = T_M_P_init_ * Tnew_old;
-    plane_.transform(T_M_P_.getRotationMatrix(),
-                     Eigen::TransformTraits::Isometry);
-    plane_.offset() = plane_.normal().dot(T_M_P_.getPosition());
-  }
-  float distSquared(const PlaneType& other) const {
-    return distFunc2(plane_.normal(), point_, other.plane_.normal(),
-                     other.point_);
-  }
-  float dist(const PlaneType& other) const { return sqrt(distSquared(other)); }
-  void buildPlaneOrientation() {
-    Eigen::Matrix3f matRotation;
-    const auto n = plane_.normal().normalized();
-    const float& x = n.x();
-    const float& y = n.y();
-    const float& z = n.z();
-    float sqrt_nx_ny = sqrt(x * x + y * y);
-    if (sqrt_nx_ny > 1e-14) {
-      matRotation << y / sqrt_nx_ny, -x / sqrt_nx_ny, 0.0, x * z / sqrt_nx_ny,
-          y * z / sqrt_nx_ny, -sqrt_nx_ny, x, y, z;
-      if (matRotation.determinant() < 0) {
-        matRotation.row(3)[0] *= -1;
-        matRotation.row(3)[1] *= -1;
-        matRotation.row(3)[2] *= -1;
-      }
-    } else {
-      matRotation << 1, 0, 0, 0, 1, 0, 0, 0, 1;
-    }
-
-    T_M_P_ = Transformation(point_, Transformation::Rotation(matRotation));
-  }
+                      const Transformation& T_M_R);
+  void transformPlane(const Transformation& Tnew_old);
+  float distSquared(const PlaneType& other) const;
+  float dist(const PlaneType& other) const;
+  void buildPlaneOrientation();
   void createPlaneSegmentAaBb(const std::vector<Point>& points,
-                              const double threshold_belongs) {
-    num_points_ = 0;
-    for (const Point& p : points) {
-      if (plane_.absDistance(p) > threshold_belongs) {
-        planeSegmentAaBb_.updateBoundingBoxLimits(p);
-        ++num_points_;
-      }
-    }
-  }
+                              const double threshold_belongs);
   void createPlaneSegmentAaBb(const std::vector<const Point*>& points,
-                              const double threshold_belongs) {
-    num_points_ = 0;
-    for (const Point* p : points) {
-      if (plane_.absDistance(*p) > threshold_belongs) {
-        planeSegmentAaBb_.updateBoundingBoxLimits(*p);
-        ++num_points_;
-      }
-    }
-  }
-
-  void setplaneSegmentAaBb(const BoundingBoxType& bbox) {
-    planeSegmentAaBb_.min = bbox.min;
-    planeSegmentAaBb_.max = bbox.max;
-  }
+                              const double threshold_belongs);
+  void setplaneSegmentAaBb(const BoundingBoxType& bbox);
 
   static float distFunc2(const Point& p1, const Point& n1, const Point& p2,
-                         const Point& n2) {
-    double d1 = (n2 - n1).squaredNorm();
-    double d2 = (p2 - p1).dot(n2 + n1);
-    return d1 + 0.5 * d2 * d2;
-  }
-  panoptic_mapping_msgs::PlaneType toPlaneTypeMsg() const {
-    panoptic_mapping_msgs::PlaneType msg;
-    msg.bbox = planeSegmentAaBb_.toBoundingBoxMsg();
-    const Eigen::Vector3d point_d = point_.cast<double>();
-    const Eigen::Vector3d normal_d = plane_.normal().cast<double>();
-    msg.class_id = class_id_;
-    tf::pointEigenToMsg(point_d, msg.point);
-    tf::pointEigenToMsg(normal_d, msg.normal);
-    tf::poseKindrToMsg(T_M_P_init_.cast<double>(), &msg.T_M_P);
-    msg.num_points_ = num_points_;
-    return msg;
-  }
-  static PlaneType fromMsg(const panoptic_mapping_msgs::PlaneType& msg) {
-    Eigen::Vector3d n_d, p_d;
-    tf::pointMsgToEigen(msg.normal, n_d);
-    tf::pointMsgToEigen(msg.point, p_d);
-    const Point n = n_d.cast<float>();
-    const Point p = p_d.cast<float>();
-    const int class_id = msg.class_id;
-    const size_t num_points = msg.num_points_;
-    kindr::minimal::QuatTransformationTemplate<double> tf_received;
-    tf::poseMsgToKindr(msg.T_M_P, &tf_received);
-    const Transformation T_M_P = tf_received.cast<float>();
-    const PlaneType ret(n, p, T_M_P, class_id);
-    return ret;
-  }
+                         const Point& n2);
+  panoptic_mapping_msgs::PlaneType toPlaneTypeMsg() const;
+  static PlaneType fromMsg(const panoptic_mapping_msgs::PlaneType& msg);
 
  private:
   PlaneID plane_id_;
